257174b9e9235adaaaae63b42dbf5ac1bd95f206
Augustus Yuan <ayuan@evernote.com>
2015-06-24 02:37:23 -0700

    started project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # slack-github-issue
 Code for Slack bot to auto comment the issue information for a specific repository when a user types the issue number.
+
+Makes use of [node-slack-client](https://github.com/slackhq/node-slack-client).
+
+## How to run
+1. Clone repo into your local machine (`https://github.com/augbog/slack-github-issue.git`)
+2. Navigate to folder (`cd slack-github-issue`)
+3. Run `npm install`
+4. Go to `index.js` and update `BOT_TOKEN`, `REPO_OWNER`, and `REPO_NAME`
+5. Run `node index.js`
---
+++ index.js
@@ -0,0 +1,69 @@
+var Client  = require('slack-client'),
+    request = require('request');
+
+module.exports = Client;
+ 
+// Create a new bot at https://YOURSLACK.slack.com/services/new/bot
+var BOT_TOKEN  = 'YOURSLACK-BOT-TOKEN',
+    REPO_OWNER = 'augbog', //i.e.
+    REPO_NAME  = 'slack-github-issue';
+
+var slack = new Client(BOT_TOKEN, true, true);
+
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+        .map(function (k) { return slack.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+ 
+    var groups = Object.keys(slack.groups)
+        .map(function (k) { return slack.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+ 
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+ 
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+ 
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+});
+ 
+// when someone posts to the channel
+slack.on('message', function(message) {
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var user = slack.getUserByID(message.user);
+    //if (message.type === 'message' && isDirect(slack.self.id, message.text)) {
+    // if we find a #...
+    if (message.type === 'message' && message.text.indexOf('#') > -1) {
+      var issueNum = message.text.substr(message.text.indexOf('#')).split(' ')[0];
+      if (/^#\d+$/.test(issueNum)) {
+        var issueDescription,
+            options = {
+              url: 'https://api.github.com/repos/' + REPO_OWNER +'/' + REPO_NAME + '/issues/' + issueNum.substr(1),
+              method: 'GET',
+              headers: {
+                'User-Agent':   'Super Agent/0.0.1',
+                'Content-Type': 'application/x-www-form-urlencoded'
+              }
+            };
+
+        //Github API requires User Agent
+        request(options, function (error, response, body) {
+          var json = JSON.parse(body);
+          if (!error && response.statusCode == 200) {
+            issueDescription = "[#" + json.number + "] " + json.title + "\n " + json.html_url;
+            channel.send(issueDescription)
+          }
+        });
+      }
+    }
+});
+ 
+slack.login();
